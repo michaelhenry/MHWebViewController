6d76a758d8531f96e170071e3f5c5f454786f528
Michael Henry <me@iamkel.net>
2019-10-01 02:26:41 +0900

    fixed for ios 13 toolbar issue. and moved out some code to a new file. (#9)
    * fixed for ios 13 toolbar issue. and moved out some code to a new file.
    * moved extensions under Sources folder
    * fixed bottom constraint.

--- MHWebViewController/Sources/MHWebViewController.swift
+++ MHWebViewController/Sources/MHWebViewController.swift
@@ -11,11 +11,28 @@ import WebKit
 
 public class MHWebViewController:UIViewController {
   
+  enum WebViewKeyPath:String {
+    case estimatedProgress
+    case title
+  }
+  
+  private lazy var container = UIView(frame: CGRect.zero)
+  private lazy var progressView = UIProgressView(progressViewStyle: .bar)
   public private(set) lazy var webView:WKWebView = WKWebView(frame: CGRect.zero)
   
-  private lazy var toolbar:UIToolbar = UIToolbar(frame: CGRect.zero)
-  private lazy var container = UIView(frame: CGRect.zero)
-  private lazy var progressView = UIProgressView(progressViewStyle: .default)
+  private lazy var toolbar:UIView = {
+    let v = UIView(frame: CGRect.zero)
+    v.isUserInteractionEnabled = true
+    v.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+    v.translatesAutoresizingMaskIntoConstraints = false
+        
+    let blurEffect = UIBlurEffect(style: .light)
+    let blurEffectView = UIVisualEffectView(effect: blurEffect)
+    v.addSubview(blurEffectView)
+    blurEffectView.bindFrameToSuperviewBounds()
+    return v
+  }()
+
   private lazy var titleLabel:UILabel = {
     let lbl = UILabel(frame: CGRect(x: 0.0, y: 0.0, width: 250.0, height: 16.0))
     lbl.adjustsFontSizeToFitWidth = true
@@ -35,9 +52,7 @@ public class MHWebViewController:UIViewController {
   }()
 
   private let topMargin:CGFloat = 10.0
-  
   private var lastLocation:CGPoint = .zero
-  
   public var request:URLRequest!
  
   public override var title: String? {
@@ -56,72 +71,99 @@ public class MHWebViewController:UIViewController {
   
   override public func loadView() {
     super.loadView()
+    setupMainLayout()
+    setupToolbar()
+  }
+  
+  override public func viewDidLoad() {
+    super.viewDidLoad()
+    addPanGestureRecognizer()
+    titleLabel.text = titleHidden ? "" : NSLocalizedString(
+      "Loading...", comment: "the loading text at the top")
+    webView.navigationDelegate = self
+    webView.load(request)
+  }
+  
+  public override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    addWebViewObservers()
+  }
+  
+  public override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    removeWebViewObservers()
+  }
+  
+  private func setupToolbar() {
+    // toolbar
+    let closeButton = createImageButton(imageName: "close_button")
+    closeButton.addTarget(self, action: #selector(dismissMe(_:)), for: .touchUpInside)
+    closeButton.tintColor = .gray
+    closeButton.widthAnchor.constraint(equalTo: closeButton.heightAnchor).isActive = true
+
+    let titleStackView = UIStackView(arrangedSubviews: [titleLabel, urlLabel])
+    titleStackView.axis = .vertical
+
+    let toolbarStackView = UIStackView(arrangedSubviews: [closeButton, titleStackView])
+    toolbarStackView.spacing = 2.0
+    toolbarStackView.axis = .horizontal
+    toolbar.addSubview(toolbarStackView)
+
+    toolbarStackView.translatesAutoresizingMaskIntoConstraints = false
+    toolbarStackView.topAnchor.constraint(equalTo: toolbar.topAnchor, constant: 5).isActive = true
+    toolbarStackView.leadingAnchor.constraint(equalTo: toolbar.leadingAnchor, constant: 5).isActive = true
+    toolbarStackView.bottomAnchor.constraint(equalTo: toolbar.bottomAnchor, constant: -5).isActive = true
+    toolbarStackView.trailingAnchor.constraint(equalTo: toolbar.trailingAnchor, constant:  -49).isActive = true
+  }
+    
+  private func createImageButton(imageName: String) -> UIButton {
+    guard let closeImage = UIImage(
+      named: imageName,
+      in: Bundle(for: MHWebViewController.self),
+      compatibleWith: nil) else { fatalError("No image named \(imageName) in the MHWebViewController.bundle") }
+    let closeButton = UIButton(type: .custom)
+    closeButton.setImage(closeImage, for: .normal)
+    return closeButton
+  }
+  
+  private func setupMainLayout() {
     view = UIView()
     view.autoresizingMask = [.flexibleHeight, .flexibleWidth]
-    view.backgroundColor = UIColor.clear
-    
-    // Setup container
+    view.backgroundColor = .clear
     view.addSubview(container)
     container.translatesAutoresizingMaskIntoConstraints = false
     container.topAnchor.constraint(
       equalTo: view.safeTopAnchor, constant: topMargin).isActive = true
-    container.heightAnchor.constraint(
-      equalTo: view.heightAnchor, constant: -topMargin - 44.0).isActive = true
+    container.bottomAnchor.constraint(
+      equalTo: view.bottomAnchor).isActive = true
     container.leadingAnchor.constraint(
       equalTo: view.safeLeadingAnchor, constant: 0).isActive = true
     container.trailingAnchor.constraint(
       equalTo: view.safeTrailingtAnchor, constant: 0).isActive = true
-    container.layer.cornerRadius = 10.0
+    container.layer.cornerRadius = 16.0
     container.clipsToBounds = true
-   
-    addPanGestureRecognizer()
-    guard let closeImage = UIImage(
-      named: "close_button",
-      in: Bundle(for: MHWebViewController.self),
-      compatibleWith: nil) else { return }
     
-    let closeButton = UIBarButtonItem(
-      image: closeImage,
-      style: .plain,
-      target: self,
-      action: #selector(dismissMe(_:)))
-    closeButton.tintColor = UIColor.darkGray
-    
-    let titleStackView = UIStackView(arrangedSubviews: [titleLabel, urlLabel])
-    titleStackView.axis = .vertical
-    let titleItem = UIBarButtonItem(customView: titleStackView)
-    
-    let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
-    toolbar.items = [closeButton, flexibleSpace, titleItem, flexibleSpace]
-  
     let mainStackView = UIStackView(arrangedSubviews: [toolbar, progressView, webView])
     mainStackView.axis = .vertical
     container.addSubview(mainStackView)
     mainStackView.bindFrameToSuperviewBounds()
   }
   
-  override public func viewDidLoad() {
-    super.viewDidLoad()
-    titleLabel.text = titleHidden ? "" : NSLocalizedString("LOADING...", comment: "the loading text at the top")
-    webView.navigationDelegate = self
-    webView.load(request)
-  }
-  
-  public override func viewDidAppear(_ animated: Bool) {
+  private func addWebViewObservers() {
     webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
     webView.addObserver(self, forKeyPath: #keyPath(WKWebView.title), options: .new, context: nil)
     webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoBack), options: .new, context: nil)
     webView.addObserver(self, forKeyPath: #keyPath(WKWebView.canGoForward), options: .new, context: nil)
   }
   
-  public override func viewDidDisappear(_ animated: Bool) {
+  private func removeWebViewObservers() {
     webView.removeObserver(self, forKeyPath:  #keyPath(WKWebView.estimatedProgress))
     webView.removeObserver(self, forKeyPath:  #keyPath(WKWebView.title))
     webView.removeObserver(self, forKeyPath:  #keyPath(WKWebView.canGoBack))
     webView.removeObserver(self, forKeyPath:  #keyPath(WKWebView.canGoForward))
   }
   
-  @objc private func dismissMe(_ sender: UIBarButtonItem) {
+  @objc private func dismissMe(_ sender: UIButton) {
     dismiss(completion: nil)
   }
   
@@ -129,21 +171,19 @@ public class MHWebViewController:UIViewController {
     dismiss(animated: true, completion: completion)
   }
   
-  override public func observeValue(
-    forKeyPath keyPath: String?,
-    of object: Any?,
+  override public func observeValue(forKeyPath keyPath: String?, of object: Any?,
     change: [NSKeyValueChangeKey : Any]?,
     context: UnsafeMutableRawPointer?) {
     
     switch keyPath {
-    case "estimatedProgress":
+    case WebViewKeyPath.estimatedProgress.rawValue:
       progressView.progress = Float(webView.estimatedProgress)
       if progressView.progress == 1.0 {
         progressView.alpha = 0.0
       } else if progressView.alpha != 1.0 {
         progressView.alpha = 1.0
       }
-    case "title":
+    case WebViewKeyPath.title.rawValue:
       title = titleHidden ? "" : webView.title
       if !titleHidden, let scheme = webView.url?.scheme,
         let host = webView.url?.host {
@@ -223,72 +263,5 @@ extension MHWebViewController:WKNavigationDelegate {
       break
     }
     decisionHandler(.allow)
-  }
-}
-
-public extension UIViewController {
-  
-  // Shortcuts
-  @objc
-  func present(urlRequest: URLRequest, titleHidden:Bool = false, completion: (() -> Void)? = nil) {
-    let web = MHWebViewController()
-    web.request = urlRequest
-    web.modalPresentationStyle = .overCurrentContext
-    web.titleHidden = titleHidden
-    present(web, animated: true, completion: completion)
-  }
-  
-  @objc
-  func present(url: URL, titleHidden:Bool = false, completion: (() -> Void)? = nil) {
-    let urlRequest = URLRequest(url: url)
-    present(urlRequest: urlRequest, titleHidden: titleHidden, completion: completion)
-  }
-}
-
-fileprivate extension UIView {
-  
-  var safeTopAnchor: NSLayoutYAxisAnchor {
-    if #available(iOS 11.0, *) {
-      return self.safeAreaLayoutGuide.topAnchor
-    }
-    return self.topAnchor
-  }
-  
-  var safeLeadingAnchor: NSLayoutXAxisAnchor {
-    if #available(iOS 11.0, *){
-      return self.safeAreaLayoutGuide.leadingAnchor
-    }
-    return self.leadingAnchor
-  }
-  
-  var safeTrailingtAnchor: NSLayoutXAxisAnchor {
-    if #available(iOS 11.0, *){
-      return self.safeAreaLayoutGuide.trailingAnchor
-    }
-    return self.trailingAnchor
-  }
-  
-  var safeBottomAnchor: NSLayoutYAxisAnchor {
-    if #available(iOS 11.0, *) {
-      return self.safeAreaLayoutGuide.bottomAnchor
-    }
-    return self.bottomAnchor
-  }
-  
-  func bindFrameToSuperviewBounds() {
-    guard let superview = self.superview else {
-      print("Error! `superview` was nil – call `addSubview(view: UIView)`")
-      return
-    }
-    
-    self.translatesAutoresizingMaskIntoConstraints = false
-    self.topAnchor.constraint(
-      equalTo: superview.topAnchor, constant: 0).isActive = true
-    self.bottomAnchor.constraint(
-      equalTo: superview.bottomAnchor, constant: 0).isActive = true
-    self.leadingAnchor.constraint(
-      equalTo: superview.leadingAnchor, constant: 0).isActive = true
-    self.trailingAnchor.constraint(
-      equalTo: superview.trailingAnchor, constant: 0).isActive = true
   }
 }
